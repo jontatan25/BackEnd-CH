2b7917d3eff8d38a29124b6b996eee83d50cdaff
Jonathan Sarmiento <jhonn.photo@gmail.com>
2022-02-01 09:45:27 -0500

    Desafio con EJS

--- EJS/server.js
+++ EJS/server.js
@@ -1,38 +1,28 @@
 const express = require('express');
 const app = express();
-const arrProductos= require('../productos')
+const productos= []
+app.use(express.urlencoded({extended:true}))
 
 app.set('view engine', 'ejs');
 
-app.get('/form',(req,res)=>{
-    const productos=[
-        {name:'Producto1'},
-        {name:'Producto2'},
-        {name:'Producto3'}
-    ];
-    const tagline= 'Products';
-    res.render('pages/index',{
+app.get('/productos',(req,res)=>{
+    const tagline= 'Productos';
+    res.render('pages/productlist',{
         productos,
         tagline
     });
 });
 
-app.get('/list-productos',(req,res)=>{
-    const productos= arrProductos;
-    const tagline= 'Productos';
-    res.render('pages/productlist',{
+app.get('/',(req,res)=>{
+    res.render('pages/index',{
         productos,
-        tagline
     });
 });
 
 app.post('/productos',(req,res)=>{
-    const productos= arrProductos;
-    const tagline= 'Productos';
-    res.render('pages/productlist',{
-        productos,
-        tagline
-    });
+    productos.push(req.body)
+    console.log(productos);
+    res.redirect('/productos')
 });
 
 const PORT=8080;
